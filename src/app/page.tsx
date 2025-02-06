aef3635b631693f0d029b5ed4d120db23ebb579c
Jorge Matos <jisapjisap@gmail.com>
2025-02-06 16:31:43 +0100

    Add SVG assets and update layout with new font and logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,8 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <p className="text-6xl font-bold">
-        Hello world
-      </p>
+      <Image src="/logo.svg" alt="Logo" width={50} height={50} />
+      <p className="text-xl font-semibold tracking-tight">NewTube</p>
     </div>
   );
 }
