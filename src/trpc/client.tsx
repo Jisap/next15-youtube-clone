47ed4c91adc385ae3d31405e621e3aee0629f95f
Jorge Matos <jisapjisap@gmail.com>
2025-02-11 19:01:31 +0100

    Add server-side tRPC client and hydration helpers for improved data handling

--- src/trpc/client.tsx
+++ src/trpc/client.tsx
@@ -13,14 +13,15 @@ export const trpc = createTRPCReact<AppRouter>();                               
 
 let clientQueryClientSingleton: QueryClient;                                       // Variable global que almacena una única instancia de QueryClient en el lado del cliente.
 
+
 function getQueryClient() {                                                        // Función que devuelve una instancia de QueryClient.
-  
   if (typeof window === 'undefined') {                                             // En el servidor siempre se devuelve una instancia de QueryClient.
     return makeQueryClient();
-  }
-  
+  } 
   return (clientQueryClientSingleton ??= makeQueryClient());                       // En el cliente se usa las instancia singleton de QueryClient.
 }
+
+
 function getUrl() {                                                                // Esta función determina la URL base para las solicitudes tRPC.  
   const base = (() => {
     if (typeof window !== 'undefined') return '';                                  // En el navegador a URL base es vacía (se usa la ruta relativa /api/trpc).
@@ -29,6 +30,7 @@ function getUrl() {                                                             
   })();
   return `${base}/api/trpc`;
 }
+
 
 export function TRPCProvider(                                                      // Componente que proporciona tanto el cliente tRPC como el QueryClient a la aplicación.
   props: Readonly<{
---
+++ src/trpc/server.tsx
@@ -0,0 +1,26 @@
+import 'server-only'; 
+
+import { createHydrationHelpers } from '@trpc/react-query/rsc';
+import { cache } from 'react';                                                     // Cache, se utiliza para evitar que se cree una nueva instancia de QueryClient en cada petición.
+import { createCallerFactory, createTRPCContext } from './init';
+import { makeQueryClient } from './query-client';
+import { appRouter } from './routers/_app';
+
+
+
+// Creación de un cliente de React Query con caché
+export const getQueryClient = cache(makeQueryClient);                              // Devuelve siempre la misma instancia de React Query dentro de la misma petición.
+
+
+// Creación de un "caller" para ejecutar procedimientos de tRPC en el servidor
+const caller = createCallerFactory(appRouter)(createTRPCContext);                  // Genera una función para llamar procedimientos de tRPC sin cliente HTTP.
+
+
+//Crea y exporta las herramientas de hidratación de tRPC para React Query en un entorno de RSC (React Server Components).  
+//- `trpc`: Cliente de tRPC adaptado a React Query, que permite realizar llamadas a los procedimientos definidos en `appRouter`.
+//- `HydrateClient`: Componente que se utiliza para hidratar los datos de React Query en el cliente después de haberlos obtenido en el servidor.
+
+export const { trpc, HydrateClient } = createHydrationHelpers<typeof appRouter>(
+  caller,        // Llamador de tRPC que permite ejecutar procedimientos directamente en el servidor
+  getQueryClient // Cliente de React Query con caché para mejorar la eficiencia de las consultas
+);
