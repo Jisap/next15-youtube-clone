fdaeb9305fa903f34d18620917a442733b61175c
Jorge Matos <jisapjisap@gmail.com>
2025-02-11 19:26:43 +0100

    Integrate tRPC client in Home component and wrap layout with TRPCProvider for improved data fetching

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,9 +1,14 @@
+"use client"
 
+import { trpc } from "@/trpc/client";
 
 export default function Home() {
+
+  const { data } = trpc.hello.useQuery({ text: "Jisap" });
+
   return (
     <div>
-      I will load videos in the future.
+      Client component says: { data?.greeting }
     </div>
   );
 }
