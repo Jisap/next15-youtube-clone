db4a3b0d4455d6735f506be695c402cbb63e6008
Jorge Matos <jisapjisap@gmail.com>
2025-02-13 18:48:02 +0100

    Implement category selection handling in CategoriesSection with URL updates

--- src/modules/home/ui/sections/categories-section.tsx
+++ src/modules/home/ui/sections/categories-section.tsx
@@ -2,6 +2,7 @@
 
 import { FilterCarousel } from "@/components/filter-carousel";
 import { trpc } from "@/trpc/client"
+import { useRouter } from "next/navigation";
 import { Suspense } from "react"
 import { ErrorBoundary } from 'react-error-boundary';
 
@@ -22,17 +23,29 @@ export const CategoriesSection = ({ categoryId }: CategoriesSectionProps) => {
 
 const CategoriesSectionSuspense = ({ categoryId }: CategoriesSectionProps) => {
 
+  const router = useRouter();
+
   const  [categories] = trpc.categories.getMany.useSuspenseQuery(); // Obtiene los datos precargados del servidor. React espera hasta que los datos estén listos (gracias a useSuspenseQuery).
   const data = categories.map((category) => ({ 
     value: category.id, 
     label: category.name 
-  }))
+  }));
+
+  const onSelect= (value:string | null) => {
+    const url = new URL(window.location.href);
+    if(value){
+      url.searchParams.set("categoryId", value);
+    }else{
+      url.searchParams.delete("categoryId");
+    }
+    router.push(url.toString()); // Establece la URL actualizada en la ruta actual con el nuevo valor de categoryId.
+  }
 
   return (
     <FilterCarousel 
       value={categoryId}
       data={data} 
-      onSelect={(x) => console.log(x)}
+      onSelect={onSelect}
     />
   )
 }
