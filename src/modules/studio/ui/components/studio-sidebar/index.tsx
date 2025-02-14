348e4844bb430985ca54aa0c5329d31693d00686
Jorge Matos <jisapjisap@gmail.com>
2025-02-14 17:22:16 +0100

    Add UserAvatar component and refactor StudioSidebar with new layout

--- src/modules/studio/ui/components/studio-sidebar/index.tsx
+++ src/modules/studio/ui/components/studio-sidebar/index.tsx
@@ -1,18 +1,54 @@
 "use client"
 
-import { Sidebar, SidebarContent } from "@/components/ui/sidebar"
-import { MainSection } from "./main-section"
+import { 
+  Sidebar, 
+  SidebarContent, 
+  SidebarGroup, SidebarMenu, 
+  SidebarMenuButton, 
+  SidebarMenuItem 
+} from "@/components/ui/sidebar"
 import { Separator } from "@/components/ui/separator"
-import { PersonalSection } from "./personal-section"
+import { LogOutIcon, VideoIcon } from "lucide-react"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { StudioSidebarHeader } from "./studio-sidebar-header"
 
 
 const StudioSidebar = () => {
+
+  const pathname = usePathname()
+
   return (
     <Sidebar className="pt-16 z-10" collapsible="icon">
-      <SidebarContent className="bg-background">
-        <MainSection />
-        <Separator />
-        <PersonalSection />
+      <SidebarContent className="bg-background">     
+        <SidebarGroup>
+          <SidebarMenu>
+            <StudioSidebarHeader />
+            <SidebarMenuItem>
+              <SidebarMenuButton 
+                tooltip="Content" 
+                asChild
+                isActive={pathname === "/studio"}  
+              >
+                <Link href="/studio">
+                  <VideoIcon className="size-5" />
+                  <span className="text-sm">Content</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+
+            <Separator />
+
+            <SidebarMenuItem>
+              <SidebarMenuButton tooltip="Exit studio" asChild>
+                <Link href="/">
+                  <LogOutIcon className="size-5" />
+                  <span className="text-sm">Exit studio</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarGroup>
       </SidebarContent>
     </Sidebar>
   )
