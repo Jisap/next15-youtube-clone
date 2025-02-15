d288882c055d6cdd599118f4a751f47223070237
Jorge Matos <jisapjisap@gmail.com>
2025-02-15 19:01:58 +0100

    Add video creation procedure and integrate with studio upload modal

--- src/modules/studio/ui/components/studio-upload-modal.tsx
+++ src/modules/studio/ui/components/studio-upload-modal.tsx
@@ -1,12 +1,24 @@
 "use client"
 
 import { Button } from "@/components/ui/button"
+import { trpc } from "@/trpc/client"
 import { PlusIcon } from "lucide-react"
 
 
 const StudioUploadModal = () => {
+
+  const utils = trpc.useUtils();                      // Accde a las utilidades de caché de tRPC
+  const create = trpc.videos.create.useMutation({
+    onSuccess: () => {
+      utils.studio.getMany.invalidate();             // Invalida la caché de la consulta -> getMany se invoca automaticamente -> se actualiza interfaz UI
+    }                                                // tRPC sabe que los datos en la caché ya no son válidos y necesita volver a ejecutar la consulta para obtener los datos más recientes.
+  })
+
   return (
-    <Button variant="secondary">
+    <Button 
+      variant="secondary"
+      onClick={() => create.mutate()}
+    >
       <PlusIcon className="size-5" />
       <span className="text-sm">Create</span>
     </Button>
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,13 +1,15 @@
-import { z } from 'zod';
+
 import { baseProcedure, protectedProcedure, createTRPCRouter } from '../init';
 import { categoriesRouter } from '@/modules/categories/server/procedures';
 import { studioRouter } from '@/modules/studio/server/procedures';
+import { videosRouter } from '@/modules/videos/server/procedures';
 
 
 
 export const AppRouter = createTRPCRouter({ // Se crea un enrutador tRPC
   categories: categoriesRouter,              // que contiene un subrouter para las categorías -> procedimientos
-  studio: studioRouter
+  studio: studioRouter,
+  videos: videosRouter
 });
 
 export type AppRouter = typeof AppRouter
