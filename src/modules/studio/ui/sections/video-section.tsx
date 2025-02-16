768a819a58f853c242064bf1ce9ffb80d5476a00
Jorge Matos <jisapjisap@gmail.com>
2025-02-16 10:14:25 +0100

    Refactor VideoSection to include Suspense and ErrorBoundary for improved loading and error handling

--- src/modules/studio/ui/sections/video-section.tsx
+++ src/modules/studio/ui/sections/video-section.tsx
@@ -2,11 +2,22 @@
 
 import { DEFAULT_LIMIT } from "@/constant"
 import { trpc } from "@/trpc/client"
-
-
+import { Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
 
 
 export const VideoSection = () => {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <VideoSectionuSuspense />
+      </ErrorBoundary>
+    </Suspense>
+  )
+}
+
+
+export const VideoSectionuSuspense = () => {
 
   const [data] = trpc.studio.getMany.useSuspenseInfiniteQuery({
     limit: DEFAULT_LIMIT
