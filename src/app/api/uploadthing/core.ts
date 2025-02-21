77438a59a3691cec99832b557917546594863e55
Jorge Matos <jisapjisap@gmail.com>
2025-02-21 17:21:58 +0100

    Add thumbnail management to file upload process

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,7 +3,7 @@ import { users, videos } from "@/db/schema";
 import { auth } from "@clerk/nextjs/server";
 import { and, eq } from "drizzle-orm";
 import { createUploadthing, type FileRouter } from "uploadthing/next";
-import { UploadThingError } from "uploadthing/server";
+import { UploadThingError, UTApi } from "uploadthing/server";
 import { z } from "zod";
 
 const f = createUploadthing();                                           // Crea una instancia de UploadThing.
@@ -19,7 +19,7 @@ export const ourFileRouter = {                                           // Defi
     .input(z.object({                                                    // 2º Validación de la entrada: videoId
       videoId: z.string().uuid(),
      }))
-    .middleware(async ({ input }) => {                                   // 3º Middleware: Validación de usuario
+    .middleware(async ({ input }) => {                                   // 3º Middleware: Validación de usuario y gestión del thumbnailKey
       const {userId: clerkUserId } = await auth();                       // Obtiene el userId de Clerk
       if (!clerkUserId) throw new UploadThingError("Unauthorized");
 
@@ -30,13 +30,43 @@ export const ourFileRouter = {                                           // Defi
 
       if(!user) throw new UploadThingError("Unauthorized");
 
-      return { user, ...input };                                         // Retorna el user junto con el videoId validado (metadata)
+      const [existingVideo] = await db                                   // Busca si el video ya existe en base de datos
+        .select({
+          thumbnailKey: videos.thumbnailKey,                             // y selecciona el campo thumbnailKey
+        })
+        .from(videos)
+        .where(and(
+          eq(videos.id, input.videoId),                                  // donde el id del video de base de datos = al id del input
+          eq(videos.userId, user.id)                                     // donde el userId del video de base de datos = el userId del usuario logueado
+        ))
+
+        if(!existingVideo){
+          throw new UploadThingError("Not found")
+        }
+
+        if(existingVideo.thumbnailKey){                                   // Si el video existe y tiene un thumbnailKey
+          const utapi = new UTApi();
+          await utapi.deleteFiles(existingVideo.thumbnailKey);            // borra el archivo de uploadthing
+          await db                                                        // Y borra la referencia en la tabla videos
+            .update(videos)
+            .set({ 
+              thumbnailKey: null,
+              thumbnailUrl: null,
+            })
+            .where(and(
+              eq(videos.id, input.videoId),
+              eq(videos.userId, user.id)
+            ))
+        }
+
+      return { user, ...input };                                         // 4º Retorna el user junto con el videoId validado (metadata)
     })
-    .onUploadComplete(async ({ metadata, file }) => {                    // 4º Acción al completar la subida del file
+    .onUploadComplete(async ({ metadata, file }) => {                    // 5º Acción al completar la subida del file
       await db                                                              // Actualiza la base de datos
         .update(videos)                                                     // actualizando en la tabla videos la dirección de la imagen devuelta por UploadThing
-        .set({                                                               
+        .set({                                                              // y el key de referencia en uploadthing       
           thumbnailUrl: file.url,
+          thumbnailKey: file.key,
         })
         .where(and(
           eq(videos.id, metadata.videoId),                                  // donde el id del video de base de datos = el id validado por UploadThing
