d9d644f358497f56089b3a81aec1c3e145044122
Jorge Matos <jisapjisap@gmail.com>
2025-02-21 15:55:46 +0100

    Add upload completion handler to invalidate video queries and close modal

--- src/modules/studio/ui/components/thumbnail-upload-modal.tsx
+++ src/modules/studio/ui/components/thumbnail-upload-modal.tsx
@@ -1,5 +1,6 @@
 import { ResponsiveModal } from "@/components/responsive-dialog";
 import { UploadDropzone } from "@/lib/uploadthing";
+import { trpc } from "@/trpc/client";
 
 
 interface ThumbnailUploadModalProps {
@@ -9,6 +10,15 @@ interface ThumbnailUploadModalProps {
 }
 
 export const ThumbnailUploadModal = ({ videoId, open, onOpenChange }: ThumbnailUploadModalProps) => {
+
+  const utils = trpc.useUtils();
+
+  const onUploadComplete = () => {
+    utils.studio.getOne.invalidate({ id: videoId });      // Invalida la consulta del video actual para actualizar el estado de la vista. 
+    utils.studio.getMany.invalidate();                    // Invalida la consulta de todos los videos del studio para actualizar el estado de la lista.
+    onOpenChange(false);                                  // Cierra el modal
+  }
+
   return (
     <ResponsiveModal 
       open={open} 
@@ -18,6 +28,7 @@ export const ThumbnailUploadModal = ({ videoId, open, onOpenChange }: ThumbnailU
       <UploadDropzone
         endpoint="thumbnailUploader"
         input={{ videoId }}
+        onClientUploadComplete={onUploadComplete}
       />
     </ResponsiveModal>
   )
