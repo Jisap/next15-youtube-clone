b8c05c0ba01fab8e4eb56b944b4087f023f993f9
Jorge Matos <jisapjisap@gmail.com>
2025-02-26 16:25:33 +0100

    Add VideoMenu component with dropdown options for sharing, adding to playlist, and removing videos

--- src/modules/videos/ui/components/video-menu.tsx
+++ src/modules/videos/ui/components/video-menu.tsx
@@ -1,8 +1,55 @@
+import { Button } from "@/components/ui/button";
+import { 
+  DropdownMenu, 
+  DropdownMenuContent, 
+  DropdownMenuItem, 
+  DropdownMenuTrigger 
+} from "@/components/ui/dropdown-menu"
+import { ListPlusIcon, MoreVertical, ShareIcon, Trash2Icon } from "lucide-react";
 
+interface VideoMenuProps {
+  videoId: string;
+  variant?: "ghost" | "secondary";
+  onRemove?: () => void;
+}
 
-const VideoMenu = () => {
+const VideoMenu = ({
+  videoId,
+  variant,
+  onRemove
+ }: VideoMenuProps) => {
+
   return (
-    <div>VideoMenu</div>
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button
+          variant={variant}
+          size="icon"
+          className="rounded-full"
+        >
+          <MoreVertical />
+        </Button>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent 
+        align="end" 
+        onClick={(e) => e.stopPropagation()}
+      >
+        <DropdownMenuItem onClick={() => {}}>
+          <ShareIcon className="mr-2 size-4" />
+          Share
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={() => { }}>
+          <ListPlusIcon className="mr-2 size-4" />
+          Add to playlist
+        </DropdownMenuItem>
+        {onRemove && (
+          <DropdownMenuItem onClick={() => { }}>
+            <Trash2Icon className="mr-2 size-4" />
+            Remove
+          </DropdownMenuItem>
+        )}
+      </DropdownMenuContent>
+    </DropdownMenu>
   )
 }
 
