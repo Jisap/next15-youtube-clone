b008706214e8d3f56038e8640e75d787b4469ebe
Jorge Matos <jisapjisap@gmail.com>
2025-03-02 16:24:32 +0100

    Add comment form component and integrate it into the video comment section

--- src/modules/videos/ui/section/comment-section.tsx
+++ src/modules/videos/ui/section/comment-section.tsx
@@ -1,17 +1,37 @@
 "use client"
 
+import { CommentForm } from "@/modules/comments/ui/components/comment-form"
 import { trpc } from "@/trpc/client"
+import { Suspense } from "react"
+import { ErrorBoundary } from "react-error-boundary"
 
 interface CommentSectionProps {
   videoId: string
 }
 
-const CommentSection = ({ videoId }: CommentSectionProps) => {
+
+export const CommentSection = ({ videoId }: CommentSectionProps) => {
+  return (
+    <Suspense fallback={<p>Loading...</p>}>
+      <ErrorBoundary fallback={<p>Error...</p>}>
+        <CommentSectionSuspense videoId={videoId} />
+      </ErrorBoundary>
+    </Suspense>
+  )
+}
+
+const CommentSectionSuspense = ({ videoId }: CommentSectionProps) => {
 
   const [comments] = trpc.comments.getMany.useSuspenseQuery({ videoId })
 
   return (
-    <div>
+    <div className="mt-6">
+      <div className="flex flex-col gap-6">
+        <h1>
+          0 Comments
+        </h1>
+        <CommentForm videoId={videoId} />
+      </div>
       {JSON.stringify(comments)}
     </div>
   )
