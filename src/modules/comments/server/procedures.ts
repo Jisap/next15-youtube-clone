8e4156c9aa4a9b42a51346089f738455565e7218
Jorge Matos <jisapjisap@gmail.com>
2025-03-03 17:45:44 +0100

    Add mutation to remove comments with user authorization and error handling

--- src/modules/comments/server/procedures.ts
+++ src/modules/comments/server/procedures.ts
@@ -1,11 +1,34 @@
 import { db } from "@/db";
 import { comments, users } from "@/db/schema";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
+import { TRPCError } from "@trpc/server";
 import { and, count, desc, eq, getTableColumns, lt, or } from "drizzle-orm";
 import { z } from "zod";
 
 
 export const commentsRouter = createTRPCRouter({
+  remove: protectedProcedure
+    .input(z.object({
+      id: z.string().uuid(),
+    }))
+    .mutation(async ({ input, ctx }) => {
+      const { id } = input;
+      const { id: userId } = ctx.user;
+
+      const [deletedComment] = await db
+        .delete(comments)
+        .where(and(
+          eq(comments.id, id),
+          eq(comments.userId, userId)
+        ))
+        .returning()
+
+      if(!deletedComment){
+        throw new TRPCError({ code: "NOT_FOUND" })
+      }
+
+      return deletedComment
+    }),
   create: protectedProcedure
     .input(z.object({
       videoId: z.string().uuid(),
