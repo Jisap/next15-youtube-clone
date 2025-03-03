343d999b01e5323b5408fcdba6ac36675bc2f6ea
Jorge Matos <jisapjisap@gmail.com>
2025-03-03 17:33:43 +0100

    Optimize comments query by fetching total count and data in parallel, ensuring accurate pagination and improved performance.

--- src/modules/comments/server/procedures.ts
+++ src/modules/comments/server/procedures.ts
@@ -39,40 +39,43 @@ export const commentsRouter = createTRPCRouter({
   .query(async({ input }) => {
     const { videoId, cursor, limit } = input;
 
-    const [totalData] = await db
-      .select({
-        count: count()                                               // Se obtiene el número total de comentarios para el video
-      })
-      .from(comments)
-      .where(eq(comments.videoId, videoId))
+    const [totalData, data ] = await Promise.all([
+      await db
+        .select({
+          count: count()                                               // Se obtiene el número total de comentarios para el video
+        })
+        .from(comments)
+        .where(eq(comments.videoId, videoId)),
 
-    const data = await db
-      .select({
-        ...getTableColumns(comments),
-        user: users,
-      })
-      .from(comments)
-      .where(and(
-        eq(comments.videoId, videoId),
-        cursor                                                       // Si hay un cursor. Este cursor se usa para obtener solo los comentarios más antiguos
-          ? or(
-              lt(comments.updatedAt, cursor.updatedAt),              // Filtra los comentarios cuya fecha de actualización (updatedAt) sea anterior (<) a la del cursor.
+      await db
+        .select({
+          ...getTableColumns(comments),
+          user: users,
+        })
+        .from(comments)
+        .where(and(
+          eq(comments.videoId, videoId),
+          cursor                                                       // Si hay un cursor. Este cursor se usa para obtener solo los comentarios más antiguos
+            ? or(
+              lt(comments.updatedAt, cursor.updatedAt),                // Filtra los comentarios cuya fecha de actualización (updatedAt) sea anterior (<) a la del cursor.
               and(
-                eq(comments.updatedAt, cursor.updatedAt),            // Si dos comentarios tienen la misma updatedAt, se usa id < cursor.id como desempate.
+                eq(comments.updatedAt, cursor.updatedAt),              // Si dos comentarios tienen la misma updatedAt, se usa id < cursor.id como desempate.
                 lt(comments.id, cursor.id)
-              ) 
+              )
             )
-          : undefined,                                               // Si no hay cursor, no se aplica filtro adicional
-      ))
-      .innerJoin(users, eq(comments.userId, users.id))               // Se añade la info del user que hizo el comentario
-      .orderBy(desc(comments.updatedAt), desc(comments.id))          // Ordena los comentarios de forma descendente por fecha de actualización y luego por id.
-      .limit(limit + 1)                                              // Se recupera limit + 1 elementos para determinar si hay más páginas disponibles.
+            : undefined,                                               // Si no hay cursor, no se aplica filtro adicional
+        ))
+        .innerJoin(users, eq(comments.userId, users.id))               // Se añade la info del user que hizo el comentario
+        .orderBy(desc(comments.updatedAt), desc(comments.id))          // Ordena los comentarios de forma descendente por fecha de actualización y luego por id.
+        .limit(limit + 1)                                              // Se recupera limit + 1 elementos para determinar si hay más páginas disponibles.
+      
+    ])
 
-    const hasMore = data.length > limit;                             // Si data contiene más elementos de los solicitados (limit), significa que hay más videos disponibles.
+    const hasMore = data.length > limit;                               // Si data contiene más elementos de los solicitados (limit), significa que hay más videos disponibles.
 
-    const items = hasMore ? data.slice(0, -1) : data;                // Si hay más elementos, se elimina el último para no enviarlo al cliente y así no superar el limit 
+    const items = hasMore ? data.slice(0, -1) : data;                  // Si hay más elementos, se elimina el último para no enviarlo al cliente y así no superar el limit 
 
-    const lastItem = items[items.length - 1];                        // Se extrae el último elemento de items para establecer el cursor de la siguiente página.
+    const lastItem = items[items.length - 1];                          // Se extrae el último elemento de items para establecer el cursor de la siguiente página.
 
     const nextCursor = hasMore
       ? {
@@ -82,7 +85,7 @@ export const commentsRouter = createTRPCRouter({
       : null
 
     return {
-      totalCount: totalData.count,
+      totalCount: totalData[0].count,
       items,
       nextCursor
     }
