653afd8ef4a5b9aadf38e3c89bd7f632107baad0
Jorge Matos <jisapjisap@gmail.com>
2025-03-13 17:53:28 +0100

    feat: implement getManySubscribed endpoint for authenticated user video retrieval with cursor and limit

--- src/modules/videos/server/procedures.ts
+++ src/modules/videos/server/procedures.ts
@@ -3,7 +3,7 @@ import { subscriptions, users, videoReactions, videos, videoViews } from "@/db/s
 import { mux } from "@/lib/mux";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { videoUpdateSchema } from '../../../db/schema';
-import { and, desc, eq, getTableColumns, inArray, isNotNull, lt, or } from "drizzle-orm";
+import { and, DBQueryConfig, desc, eq, getTableColumns, inArray, isNotNull, lt, or } from "drizzle-orm";
 import { TRPCError } from "@trpc/server";
 import { z } from "zod";
 import { UTApi } from "uploadthing/server";
@@ -13,7 +13,98 @@ import { workflow } from "@/lib/workflow";
 
 
 export const videosRouter = createTRPCRouter({
-  getManyTrending: baseProcedure                                          // Indica que este endpoint no requiere autenticación.
+  getManySubscribed: protectedProcedure                                 // Indica que este endpoint si requiere autenticación.
+    .input(                                                             // Valida los parámetros de entrada:
+      z.object({
+        cursor: z.object({                                              // 1º Cursor que es un objeto con:
+          id: z.string().uuid(),                                        // Identificador del último video cargado.
+          updatedAt: z.date()                                           // Fecha de actualización del último video cargado.  
+        })
+          .nullish(),
+        limit: z.number().min(1).max(100),                              // Y 3º Limit que es el número de videos a recuperar 
+      })
+    )
+    .query(async ({ input,ctx }) => {                                   // Validados los datos se procede a la consulta a la base de datos.
+
+      const { cursor, limit } = input;                                  // Se extraen los valores de input (cursor, limit).
+      const { id: userId } = ctx.user;                                  // Obtenemos el id de usuario autenticado desde el ctx.
+      
+      const viewerSubcription = db.$with("viewer_subscriptions").as(    // Creamos una CTE con los creadores a los que el usuario está suscrito.
+        db
+          .select({
+            userId: subscriptions.creatorId,                            // Seleccionamos los ids de los creadores de videos
+          })
+          .from(subscriptions)                                          // desde la tabla subscriptions
+          .where(
+            eq(subscriptions.viewerId, userId)                          // Filtramos solo las suscripciones donde el usuario autenticado es el seguidor.
+          )
+      );
+
+      // Subscriptions
+      // id	creatorId	viewerId     userId = 201
+      // 1	  101	      201           result     [ { userId: 101 },
+      // 2	  102	      201                        { userId: 102 },
+      // 3	  103	      202          
+      // 4	  104	      201                        { userId: 101 }, ]
+
+      const data = await db
+        .select({                                                       // De la tabla videos se seleccionan los campos:
+          ...getTableColumns(videos),                                   // props relativas a la tabla videos.                              
+          user: users,                                                  // se agrega la relacion de usuarios
+          viewCount: db.$count(                                         // videoCount: número de visualizaciones del video.                                
+            videoViews, eq(videoViews.videoId, videos.id)
+          ),
+          likeCount: db.$count(                                         // likeCount: número de likes del video.
+            videoReactions, and(                                              // En la tabla videoReactions se filtran los likes.
+              eq(videoReactions.videoId, videos.id),                          // correspondientes al video
+              eq(videoReactions.type, "like")                                 // con el tipo de reaction "like".
+            )),
+          dislikeCount: db.$count(                                      // dislikeCount: número de dislikes del video.
+            videoReactions, and(                                              // En la tabla videoReactions se filtran los likes.
+              eq(videoReactions.videoId, videos.id),                          // correspondientes al video
+              eq(videoReactions.type, "dislike")                              // con el tipo de reaction "dislike".
+            ))
+        })
+        .from(videos)                                                   // Solo se obtienen los videos 
+        .innerJoin(users, eq(videos.userId, users.id))                  // (se agrega la relación de usuarios)
+        .innerJoin(                                                     // Agregamos la lista de creadores a los que el usuario está suscrito.
+          viewerSubcription,
+          eq(viewerSubcription.userId, users.id)                              // Incluimos los users cuyos is = creatorId 
+        )
+        .where(and(
+          eq(videos.visibility, "public"),                              // que sean públicos (visibility = "public")
+          cursor                                                        // Y si hay un cursor, (Este cursor se usa para obtener solo los videos más antiguos)
+            ? or(
+              lt(videos.updatedAt, cursor.updatedAt),                   // filtra los videos cuya fecha de actualización (updatedAt) sea anterior (<) a la del cursor.
+              and(
+                eq(videos.updatedAt, cursor.updatedAt),                 // Si dos videos tienen la misma updatedAt, se usa id < cursor.id como desempate.
+                lt(videos.id, cursor.id)
+              )
+            )
+            : undefined,
+        )).orderBy(desc(videos.updatedAt), desc(videos.id))             // Se ordena en orden descendente por updatedAt y luego por id.
+        .limit(limit + 1)                                               // Se recupera limit + 1 elementos para determinar si hay más páginas disponibles.
+
+
+      const hasMore = data.length > limit;                              // Si data contiene más elementos(limit+1) de los solicitados (limit), significa que hay más videos disponibles.
+
+      const items = hasMore ? data.slice(0, -1) : data;                 // Si hay más elementos, se elimina el último para no enviarlo al cliente y así no superar el limit 
+
+      const lastItem = items[items.length - 1];                         // Se extrae el último elemento de items para establecer el cursor de la siguiente página.
+
+      const nextCursor = hasMore                                          // Si hasMore = true se crea un objeto nextCursor con el id y updatedAt del lastItem
+        ? {
+          id: lastItem.id,
+          updatedAt: lastItem.updatedAt,
+        }
+        : null
+
+      return {
+        items,
+        nextCursor, // Este cursor se utilizará en la próxima solicitud para obtener los siguientes videos.
+      }
+    }),
+  getManyTrending: baseProcedure                                      // Indica que este endpoint no requiere autenticación.
     .input(                                                           // Valida los parámetros de entrada:
       z.object({  
         cursor: z.object({                                            // 1º Cursor que es un objeto con:
