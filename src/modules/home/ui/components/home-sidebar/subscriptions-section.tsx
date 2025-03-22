449bde3ba46fd534d92d5d0853d458af73f9184a
Jorge Matos <jisapjisap@gmail.com>
2025-03-22 16:33:48 +0100

    feat: add loading skeleton and navigation link for subscriptions section

--- src/modules/home/ui/components/home-sidebar/subscriptions-section.tsx
+++ src/modules/home/ui/components/home-sidebar/subscriptions-section.tsx
@@ -1,32 +1,48 @@
 "use client"
 
 import { SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarMenu, SidebarMenuButton, SidebarMenuItem } from "@/components/ui/sidebar"
+import { Skeleton } from "@/components/ui/skeleton"
 import UserAvatar from "@/components/user-avatar"
 import { DEFAULT_LIMIT } from "@/constant"
 import { trpc } from "@/trpc/client"
+import { ListIcon, Sidebar } from "lucide-react"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
 
-
+export const LoadingSkeleton = () => {
+  return (
+    <>
+      {[1,2,3,4].map((i) => (
+        <SidebarMenuItem key={i}>
+          <SidebarMenuButton disabled> 
+            <Skeleton className="size-6 rounded-full shrink-0" />
+            <Skeleton className="h-4 w-full" />
+          </SidebarMenuButton>
+        </SidebarMenuItem>
+      ))}
+    </>
+  )
+}
 
 export const SubscriptionsSection = () => {
 
   const pathname = usePathname();
-  const { data } = trpc.subscriptions.getMany.useInfiniteQuery(
+  const { data, isLoading } = trpc.subscriptions.getMany.useInfiniteQuery(
     {
       limit: DEFAULT_LIMIT
     },
     {
       getNextPageParam: (lastPage) => lastPage.nextCursor,
     }
-  )
+  );
 
   return (
     <SidebarGroup>
       <SidebarGroupLabel>Subscriptions</SidebarGroupLabel>
       <SidebarGroupContent>
         <SidebarMenu>
-          {data?.pages.flatMap((page) => page.items).map((subscription) => (
+          {isLoading && <LoadingSkeleton />}
+          {!isLoading && data?.pages.flatMap((page) => page.items).map((subscription) => (
             <SidebarMenuItem key={`${subscription.creatorId}-${subscription.viewerId}`}>
               <SidebarMenuButton
                 tooltip={subscription.user.name}
@@ -44,6 +60,20 @@ export const SubscriptionsSection = () => {
               </SidebarMenuButton>
             </SidebarMenuItem>
           ))}
+
+          {!isLoading && (
+            <SidebarMenuItem>
+              <SidebarMenuButton
+                asChild
+                isActive={pathname === "/subscriptions"}
+              >
+                <Link href="/subscriptions" className="flex items-center gap-4">
+                  <ListIcon className="size-4 "/>
+                  <span className="text-sm">All subscriptions</span>
+                </Link>
+              </SidebarMenuButton>
+            </SidebarMenuItem>
+          )}
         </SidebarMenu>
       </SidebarGroupContent>
     </SidebarGroup>
